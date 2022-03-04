17258ca59af9d5072d8b81a0375425f735e146e0
thoonk78 <100811955+thoonk78@users.noreply.github.com>
2022-03-04 15:58:05 -0600

    Add files via upload

--- Project1.java
+++ Project1.java
@@ -4,8 +4,8 @@ import java.util.Random;
  * See the project description for details about what is expected
  * for the methods outlind below.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Samuel Ayoade & Keith Thoong
+ * @version Thursday March 4th, 2022
  */
 public class Project1 {
 
@@ -32,58 +32,38 @@ public class Project1 {
 
         System.out.println("Done. Normal termination.");
     }
-
+    /**
+     * Generates a randomboard sized exp by exp.
+     *
+     * @param exp Takes in user's exponent
+     * @return returns user 2d generated board
+     */
     public static int[][] getRandomBoard(int exp) {
         Random rand = new Random();
 
         // Your code goes here.
         int boardSize = (int)(Math.pow(2,exp));
-
         int[][] randomBoard = new int[boardSize][boardSize];
-
+        
         randomBoard[rand.nextInt(boardSize)][rand.nextInt(boardSize)] = 1;
-        //randomBoard[0][0] = 2;
 
         // Don't forget to increment counter in here.
         counter++;
         return randomBoard;
     }
-
-    /*
-    public static int[][] getRandomBoard(int exp) {
-    // Your code goes here.
-    int[][] board = new int[8][8];
-    Random rand = new Random();
-    int firstTileRow = 5;
-    int firstTileColumn = 6;
-    board[5][6] = counter;
-    counter++;
-    return board;
-    // Don't forget to increment counter in here.
-    }
-
-    public static void printBoard(int[][] board) {
-    for(int i = 0; i < 8; i++)
-    {
-    System.out.println("+--+--+--+--+--+--+--+--");
-    for(int j = 0; j < 8; j++)
-    {
-    System.out.printf("|%2d|", board[i][j]);
-    }
-    System.out.println();
-    }
-    }
+    /**
+     * Prints Random generated boar
+     *
+     * @param board the random generated board
      */
-
-    //i addded this ..i replaced the prints cuz previous code wasnt printing for a 2 x 2 array
     public static void printBoard(int[][] board) {
         // Your code goes here.
-
+        int width = Math.max((counter / 10), 1);
         for(int i = 0; i < board.length; i++){
             printBorders(board);
 
             for(int j = 0; j < board.length; j++){
-                System.out.printf("|%d", board[i][j]);
+                System.out.printf("|%" + width + "d", board[i][j]);
             }
             System.out.println("|");
 
@@ -91,120 +71,99 @@ public class Project1 {
         printBorders(board);
         System.out.println();
     }
-
+    /**
+     * Prints borders around the numbers
+     *
+     * @param boardLength the length of random genreted board
+     */
     public static void printBorders(int[][] boardLength){
-
         for(int x = 0; x < boardLength.length; x++){
-            System.out.printf("+-");
+            System.out.print("+");
+            for(int y = 0; y < Math.max((counter / 10), 1); y++)
+            {
+                System.out.print("-");   
+            }
         }
-
         System.out.println("+");
     }
-
+    /**
+     * calls on helper method
+     *
+     * @param randomly generated board 
+     */
     public static void tileCheckerboardRec(int[][] board) {
-        // Your code goes here.
         tileCheckerboardRec(board, 0, board.length, 0, board.length);
     }
-
+    /**
+     * Helper method for tileCheckerboardRec
+     *
+     * @param board Randomly generated board
+     * @param left Start point for Column
+     * @param right End point for Column
+     * @param top Start point for Row
+     * @param bottom End point for Row
+     */
     private static void tileCheckerboardRec(int[][] board, int left,
-    int right,
-    int top,
-    int bottom) {
-        // Your code goes here.
-
-        //i addded this 
+                                                           int right,
+                                                           int top,
+                                                           int bottom) 
+    {
         int midRow = (top + bottom) / 2;
         int midColumn = (right + left) / 2;
         
-        boolean quadrant1Empty = isQuadrantEmpty(board, left, (right / 2), top, (bottom / 2));
-        boolean quadrant2Empty = isQuadrantEmpty(board, (right / 2) + 1, right, top, (bottom / 2));
-        boolean quadrant3Empty = isQuadrantEmpty(board, left, (right / 2), (bottom / 2), bottom);
-        boolean quadrant4Empty = isQuadrantEmpty(board, (right / 2), right, (bottom / 2), bottom);
-        //also try if (right - left > 1);
-        if(right - left == 1 )
+        if(right - left > 1 )
         {
+            //Checks if each quadrant is empty
+            boolean quadrant1Empty = isQuadrantEmpty(board, left, midColumn, top, midRow);
+            boolean quadrant2Empty = isQuadrantEmpty(board, midColumn, right, top, midRow);
+            boolean quadrant3Empty = isQuadrantEmpty(board, left, midColumn, midRow, bottom);
+            boolean quadrant4Empty = isQuadrantEmpty(board, midColumn, right, midRow, bottom);
+            
+            //fills in the innermost space of the quadrant if it is empty
             if(quadrant1Empty)
             {
-                board[(bottom / 2) - 1][(right / 2) - 1] = counter;
-                //counter++;
-                //tileCheckerboardRec(board, left, right / 2, top, bottom / 2);
+                board[midRow - 1][midColumn - 1] = counter;
             }
             if(quadrant2Empty)
             {
-                board[(bottom / 2) - 1][(right / 2)] = counter;    
-                //counter++;
-                //tileCheckerboardRec(board, (right / 2), right, top, bottom / 2);
+                board[midRow - 1][midColumn] = counter;    
             }
             if(quadrant3Empty)
             {
-                board[(bottom / 2)][(right / 2) - 1] = counter;    
-                //counter++;
-                //tileCheckerboardRec(board, left, right / 2, bottom / 2, bottom);
+                board[midRow][midColumn - 1] = counter;    
             }
             if(quadrant4Empty)
             {
-                board[(bottom / 2)][(right / 2)] = counter;    
-                //counter++;
-                //tileCheckerboardRec(board, right / 2, right, bottom / 2, bottom);
+                board[midRow][midColumn] = counter;    
             }
             counter++;
             
-            if(quadrant1Empty)
-            {
-                tileCheckerboardRec(board, left, right / 2, top, bottom / 2);
-            }
-            else if(quadrant2Empty)
-            {
-                tileCheckerboardRec(board, (right / 2), right, top, bottom / 2);
-            }
-            else if(quadrant3Empty)
-            {
-                tileCheckerboardRec(board, left, right / 2, bottom / 2, bottom);
-            }
-            else if(quadrant4Empty)
-            {
-                tileCheckerboardRec(board, right / 2, right, bottom / 2, bottom);
-            }
-        }
-        
-        
-        
-    }
-    
-    /*
-    public static boolean isQuadrantEmpty(int[][] board, int left,
-    int right,
-    int top,
-    int bottom) {
-    int colSearched = left;
-    int rowSearched = top;
-    if(board[rowSearched][colSearched] > 0)
-    {
-    System.out.println("\n");
-    printBoard(board);
-    return false;
-    }
-    else if(colSearched + 1 < right)
-    {
-    return isQuadrantEmpty(board, colSearched + 1, right, top, bottom);
-    }
-    else if(rowSearched + 1 < bottom)
-    {
-    return isQuadrantEmpty(board, left, right, rowSearched + 1, bottom);   
-    }
-    else
-    {
-    System.out.println("\n");
-    printBoard(board);
-    return true;   
-    }
-    }
-     */
+            //divide into smaller quadrants and check again
+            tileCheckerboardRec(board, left, midColumn, top, midRow);
 
+            tileCheckerboardRec(board, midColumn, right, top, midRow);
+            
+            tileCheckerboardRec(board, left, midColumn, midRow, bottom);
+            
+            tileCheckerboardRec(board, midColumn, right, midRow, bottom);
+
+        }
+    }
+    /**
+     * Checks if quadrant is empty
+     *
+     * @param board Randomly generated board
+     * @param left Start point for Column
+     * @param right End point for Column
+     * @param top Start point for Row
+     * @param bottom End point for Row
+     * @return Returns true or false depending on whether the the quadrant is empty or not
+     */
+    
     public static boolean isQuadrantEmpty(int[][] board, int left,
-    int right,
-    int top,
-    int bottom) {
+                                                         int right,
+                                                         int top,
+                                                         int bottom) {
         // Your code goes here.
         boolean isQuadrantEmpty = true;
         for(int i = top; i < bottom; i++){
@@ -213,10 +172,8 @@ public class Project1 {
                     isQuadrantEmpty = false;
                     //counter++;
                 }
-
             }
         }
         return isQuadrantEmpty;
     }
-
 }
